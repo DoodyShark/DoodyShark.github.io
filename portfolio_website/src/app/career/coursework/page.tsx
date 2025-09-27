574ef0676e6db759c4d18a65796ba669f6567093
DoodyShark <da2863@nyu.edu>
2025-09-27 05:14:39 +0200

    fixed apostrophe

--- portfolio_website/src/app/career/coursework/page.tsx
+++ portfolio_website/src/app/career/coursework/page.tsx
@@ -4,9 +4,9 @@ export default function CourseworkPage() {
   return (
     <div className="w-full">
       <h1 className="text-3xl font-bold mb-6">Coursework</h1>
-      <h2 className="text-xl font-bold mb-6">Master's Coursework</h2>
+      <h2 className="text-xl font-bold mb-6">Master&apos;s Coursework</h2>
       <MarkdownCards row_width = {3} file="masters-courses.json" href_path="/career/coursework/read"/>
-      <h2 className="text-xl font-bold mt-6 mb-6">Bachelor's Coursework</h2>
+      <h2 className="text-xl font-bold mt-6 mb-6">Bachelor&apos;s Coursework</h2>
       <MarkdownCards row_width = {3} file="bachelors-courses.json" href_path="/career/coursework/read"/>
     </div>
   );
